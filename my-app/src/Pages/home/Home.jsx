3f6dba77fcdce6b7189ae63a67882fbff842906a
Saumy Aryan <saumyaryan10@gmail.com>
2024-02-11 11:55:09 +0530

    list of properties added

--- my-app/src/Pages/home/Home.jsx
+++ my-app/src/Pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import "./home.css"
 import Navbar from "../../components/Navbar/navbar"
 import Header from "../../components/Header/header"
-
+import Featured from "../../components/Featured/featured"
+import PropertyList from "../../components/PropertyList/propertyList"
 export const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header/>  
+      <div className="homeContainer">
+        <Featured/>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+      </div>
     </div>
 
   )
