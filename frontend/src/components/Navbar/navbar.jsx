15a516e6105b4ae03c2a31ccb005d76d0b2137a3
Saumy Aryan <saumyaryan10@gmail.com>
2024-02-17 02:20:19 +0530

    adding authentication

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,18 +1,25 @@
-import "./navbar.css"
-// import header from "../Header/header"
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
 
-export const navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">lamabooking</span>
+        </Link>
+        {user ? user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default Navbar;
